7900abbb22e210090b41fbcd86d8c4cc39dd410e
Lydia Kipkorir <ljjepkoech@usiu.ac.ke>
2024-06-10 19:10:00 +0300

    Made a few changes to the UI of this code

--- app.py
+++ app.py
@@ -81,9 +81,9 @@ if selected == 'Diabetes Prediction':
         diab_prediction = diabetes_model.predict([user_input])
 
         if diab_prediction[0] == 1:
-            diab_diagnosis = 'The person is diabetic'
+            diab_diagnosis = 'This person is diabetic'
         else:
-            diab_diagnosis = 'The person is not diabetic'
+            diab_diagnosis = 'This person is not diabetic'
 
     st.success(diab_diagnosis)
 
@@ -148,9 +148,9 @@ if selected == 'Heart Disease Prediction':
         heart_prediction = heart_disease_model.predict([user_input])
 
         if heart_prediction[0] == 1:
-            heart_diagnosis = 'The person is having heart disease'
+            heart_diagnosis = 'This person has heart disease'
         else:
-            heart_diagnosis = 'The person does not have any heart disease'
+            heart_diagnosis = 'This person does not have heart disease'
 
     st.success(heart_diagnosis)
 
@@ -243,8 +243,8 @@ if selected == "Parkinsons Prediction":
         parkinsons_prediction = parkinsons_model.predict([user_input])
 
         if parkinsons_prediction[0] == 1:
-            parkinsons_diagnosis = "The person has Parkinson's disease"
+            parkinsons_diagnosis = "This person has Parkinson's disease"
         else:
-            parkinsons_diagnosis = "The person does not have Parkinson's disease"
+            parkinsons_diagnosis = "This person does not have Parkinson's disease"
 
     st.success(parkinsons_diagnosis)
